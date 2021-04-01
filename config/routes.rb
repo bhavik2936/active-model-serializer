81cda2a974697e5f4703269f8b8fd48637eecfb6
Bhavik Parmar <bhavik2936@gmail.com>
2021-04-01 18:43:05 +0530

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  concern :api_base do
+    resources :employees, only: [:index, :create, :update, :destroy, :show]
+    resources :managers, only: [:index, :create, :update, :destroy, :show]
+  end
+
+  namespace :v1 do
+    concerns :api_base
+  end
 end
