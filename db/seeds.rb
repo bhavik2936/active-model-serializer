69b4ccd685b51cd9204a9153e15acd403c40eead
Bhavik Parmar <bhavik2936@gmail.com>
2021-04-02 00:28:28 +0530

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Employee.destroy_all
+Manager.destroy_all
+
+managers = (1..20).map do
+  Manager.create!(
+    name: Faker::Name.name
+  )
+end
+
+employee = (1..50).map do
+  Employee.create!(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    dob: Faker::Date::birthday(min_age: 18, max_age: 65),
+    mobile: Faker::Number.number(digits: 10),
+    designation: Faker::Lorem.word,
+    salary: Faker::Number.positive,
+    manager: managers.sample
+  )
+end
